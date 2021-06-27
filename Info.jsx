63098089c4add3ada2aec8e4e1cacdd98f9d8b70
mansh10 <73669075+mansh10@users.noreply.github.com>
2021-06-28 00:17:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Iron Man</h1>
+      <p>
+        A hero could have very many different definitions, like it could be a
+        role model of yours, or it could be a hero from ancestry. My definition
+        of a hero would be someone that has super natural abilities and uses
+        those abilities to help keep people from damage. There are many
+        different heroes in the world and i chose to write about Iron Man
+        because not only is he an awesome hero, but he is also a very successful
+        person, being the owner of Starks Industry, and making it better by
+        improving the weapons they make. At the same time that he is saving the
+        world he is also a very hard working person
+      </p>
+    </div>
+  );
+}
+export default Info;
